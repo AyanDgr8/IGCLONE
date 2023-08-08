7039da027d27f5db7dadd3705520ce945247e3a4
Ayan Khan <ayandgr5@gmail.com>
2023-08-08 14:12:12 +0530

    major changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path'); // Make sure to require 'path'
 
 const app = express();
 const port = 5000;
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI)
         console.log("Connection to DB failed!", err);
     });
     
-  const postsRoutes = require('../routes/post');
-  app.use(postsRoutes);
-  
-  app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+const postsRoutes = require('./routes/posts'); // Update the path to include ./routes/posts
+app.use(postsRoutes);
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
